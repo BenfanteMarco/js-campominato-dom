9e57360fa3f3a3e9114f9e40a295413d41752362
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2023-12-02 18:48:01 +0100

    added script that generate random number for bombs

--- js/script.js
+++ js/script.js
@@ -8,33 +8,101 @@ playBtn.addEventListener('click', function(){
 })
 
 // funzione che crea la grafica della cell
-function createCell(num){
+function createCell(num, cells_in_row){
     // create div class = square
     const square = document.createElement('div');
     square.classList.add('square');
-    square.innerText = num;
+    square.style.width = `calc(100% / ${cells_in_row})`;
+    square.style.height = square.style.width;
 
+    square.innerText = num;
+    
     return square
+}
+
+// funzione creo la grid
+function createGrid(cells_number, cells_in_row){
+    // crea la grid con le cell
+    for(let i=1; i<=cells_number; i++){
+        // create a cell
+        let cell = createCell(i, cells_in_row);
+
+        // quando clicchi la cell si colora (metterlo prima dell'appenChild)
+         cell.addEventListener('click', function(){
+            this.classList.add('clicked');
+            console.log('Cell number: ' + this.innerText)
+         })
+
+        // add cell to grid
+        grid.appendChild(cell); 
+    }
 }
 
 // funzione per generare la grid
 function createNewGame(){
     // take grid from html
     const grid = document.getElementById('grid');
+    
+    // take select from html
+    const difficulty = document.getElementById('difficulty');
+    // dichiaro variabile collegata alla value
+    let level = parseInt(difficulty.value);
+    
+    // selezione della difficoltà
+    
+    // dichiaro i numberi dlle cell in base alla difficoltà
+    let cellsNumber;
+    let cellsPerRow;
+    
+    // utilizzio switch per la selezione del livello (rivedi a che serve switch) (switch serve per non utilizzare per forza if else)
+    switch(level){
+        case 1:
+            cellsNumber = 100;
+            break;
+        case 2:
+            cellsNumber = 81;
+            break;
+        case 3:
+            cellsNumber = 49;
+            break; 
+    }
+
+    // faccio la radice quadrata al contrario
+    cellsPerRow = Math.sqrt(cellsNumber); // quindi la radie di 81 è 9
 
     // fare in modo che crea solo una griglia e non di più
     grid.innerHTML = '';
 
-    for(let i=1; i<=100; i++){
-        // create a cell
-        let cell = createCell(i);
+    // chiamata funzione che crea la grid
+    createGrid(cellsNumber, cellsPerRow);
+}
 
-        // quando clicchi la cell si colora (metterlo prima dell'appenChild)
-         cell.addEventListener('click', function(){
-            this.classList.add('clicked');
-         })
+// funzione che genera le bombe
+function generateBombsList(NUMBER_OF_BOMBS){ // scritto in maiuscolo perchè è una costante il numero di bombe
+    // array inizialmete vuoto
+    let bombs = [];
 
-        // add cell to grid
-        grid.appendChild(cell); 
+    for(let i=0; i<NUMBER_OF_BOMBS; i++){
+        // inserire nell'array vuoto un numero casuale
+        // richiamo funzione che genera il numero unico
+        bombs.push(generateUniqueNumber(bombs));
     }
+}
+
+// funzione che genera un numero UNICO casuale per le bombe
+function generateUniqueNumber(array_bombs){
+    let checkNumber = false;
+    let randomInt;
+
+    while(checkNumber == false){ // oppure solo (!checkNumber)
+        // genera numero
+        randomInt = Math.floor(Math.random() * 100 + 1);
+
+        // verifico che il numero non sia uguale
+        if(array_bombs.includes(randomInt) == false){
+            checkNumber = true;
+        }
+    }
+
+    return randomInt;
 }
